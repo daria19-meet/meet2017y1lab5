f8648d1786d8d6a265f0b15032f7b93d51ef7d0c
daria19-meet <daria19@meet.mit.edu>
2017-07-30 13:51:49 +0300

    Stopping here

---
+++ fib.py
@@ -0,0 +1,4 @@
+def fib (n):
+    for i in range(3,n+1):
+        c=c+d
+        
--- fun2.py
+++ fun2.py
@@ -1,5 +1,12 @@
-def draw_1d(n,c):
-    print(c*n)
-
+def special_draw_2d(c,n,d,s):
+    a=0
+    n=n-2
+    print(c*d)
+    a=c-2
+    for i in range(n):
+        print(d+a*s+d)
+    print(c*d)
+    
+        
 
 
